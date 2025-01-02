65756b3e8c224908cf0271be3d4c16e46950c476
chabugon <qnrhs0913@gmail.com>
2025-01-02 10:36:36 +0900

    [feat]api_key dotenv 패키지로 .env사용

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_movie_app/presentation/pages/home/home_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-   runApp(const ProviderScope(child: MyApp()));
+void main() async{
+  await dotenv.load(fileName: '.env');
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
